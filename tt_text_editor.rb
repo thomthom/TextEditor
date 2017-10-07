7db8573c2ef347462e9e992cc10e936b691280ee
Thomas Thomassen <thomas@thomthom.net>
2017-10-07 13:43:44 +0200

    Bump version and copyright.

--- tt_text_editor.rb
+++ tt_text_editor.rb
@@ -16,27 +16,30 @@ module TT
   
   ### CONSTANTS ### ------------------------------------------------------------
   
+  file = __FILE__.dup
+  file.force_encoding('UTF-8') if file.respond_to?(:force_encoding)
+
   # Plugin information
   PLUGIN_ID       = 'TT_TextEditor'.freeze
   PLUGIN_NAME     = '3D Text Editor'.freeze
-  PLUGIN_VERSION  = '1.1.5'.freeze
+  PLUGIN_VERSION  = '1.2.0'.freeze
 
   # Resource paths
-  FILENAMESPACE = File.basename( __FILE__, '.rb' )
-  PATH_ROOT     = File.dirname( __FILE__ ).freeze
-  PATH          = File.join( PATH_ROOT, FILENAMESPACE ).freeze
+  FILENAMESPACE = File.basename(file, '.*')
+  PATH_ROOT     = File.dirname(file).freeze
+  PATH          = File.join(PATH_ROOT, FILENAMESPACE).freeze
   
   
   ### EXTENSION ### ------------------------------------------------------------
   
   unless file_loaded?( __FILE__ )
-    file = File.join( PATH, 'core.rb' )
-    ex = SketchupExtension.new( PLUGIN_NAME, file )
+    loader = File.join(PATH, 'core')
+    ex = SketchupExtension.new(PLUGIN_NAME, loader)
     ex.description = "Editable 3D text with live preview."
     ex.version     = PLUGIN_VERSION
-    ex.copyright   = 'Thomas Thomassen © 2012–2013'
+    ex.copyright   = 'Thomas Thomassen © 2012–2017'
     ex.creator     = 'Thomas Thomassen (thomas@thomthom.net)'
-    Sketchup.register_extension( ex, true )
+    Sketchup.register_extension(ex, true)
   end 
 
   end # module Editor3dText
